2bca86b551be7e2fe242a9f3cafbac2f0defd202
Lvdzandt <lucvdzandt@gmail.com>
2022-03-23 13:01:16 +0100

    docker-compose

--- src/main/java/com/example/fhict/GatewayApplication.java
+++ src/main/java/com/example/fhict/GatewayApplication.java
@@ -2,6 +2,9 @@ package com.example.fhict;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -9,5 +12,4 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
 }
