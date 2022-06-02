0ccb41d14c93d0dcc32acf6b5c5d535abd8f2491
Lvdzandt <lucvdzandt@gmail.com>
2022-06-02 10:36:53 +0200

    added k8s files + docker compose

--- src/main/java/com/example/fhict/GatewayApplication.java
+++ src/main/java/com/example/fhict/GatewayApplication.java
@@ -1,13 +1,22 @@
 package com.example.fhict;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 
 @SpringBootApplication
 public class GatewayApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
