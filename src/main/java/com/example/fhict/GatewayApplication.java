350fa87fb023670e1cb3353d782a0855dee6dc5a
Lvdzandt <lucvdzandt@gmail.com>
2022-08-18 13:31:57 +0200

    added keycloak integration

--- src/main/java/com/example/fhict/GatewayApplication.java
+++ src/main/java/com/example/fhict/GatewayApplication.java
@@ -1,17 +1,7 @@
 package com.example.fhict;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 
 @SpringBootApplication
